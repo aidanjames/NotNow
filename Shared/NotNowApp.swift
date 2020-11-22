2089675e0e8e11ea14dfa0b4573266f750957ce0
AidanJP <aidanpendlebury@gmail.com>
2020-11-22 15:29:52 +0000

    Initial Commit

---
+++ Shared/NotNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotNowApp.swift
+//  Shared
+//
+//  Created by Aidan Pendlebury on 22/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct NotNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

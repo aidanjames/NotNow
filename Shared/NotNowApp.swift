7fc1cf700ca463f0b8078471671b8278c8a66b5b
AidanJP <aidanpendlebury@gmail.com>
2020-12-24 13:02:07 +0000

    Removed testing button and moved the navigation view out to the main app view.

--- Shared/NotNowApp.swift
+++ Shared/NotNowApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct NotNowApp: App {
     var body: some Scene {
         WindowGroup {
-            AllRemindersView()
+            NavigationView {
+                AllRemindersView()
+            }
+            .accentColor(Color(Colours.hotCoral))
         }
     }
 }
--- Shared/Views/AllRemindersView/AllRemindersView.swift
+++ Shared/Views/AllRemindersView/AllRemindersView.swift
@@ -24,77 +24,69 @@ struct AllRemindersView: View {
     }
     
     var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVStack {
-                    ForEach(viewModel.allRemindersSorted) { reminder in
-                        ReminderListView(viewModel: viewModel, showingSnoozeActionSheet: $showingSnoozeActionSheet, reminder: reminder, height: 120)
-                            .alert(isPresented: $showingDeleteWarning) {
-                                Alert(
-                                    title: Text("Are you sure you want to delete this?"),
-                                    message: Text("There is no undo"),
-                                    primaryButton: .destructive(Text("Delete")) {
-                                        withAnimation {
-                                            viewModel.deleteNotificationsForReminder(id: viewModel.tappedReminder!)
-                                        }
-                                    },
-                                    secondaryButton: .cancel())
-                            }
-                            .actionSheet(isPresented: $showingSnoozeActionSheet) {
-                                ActionSheet(title: Text("Select an option"), message: nil, buttons: [
-                                    .destructive(Text("Delete '\(tappedReminderTitle)'")) { showingDeleteWarning.toggle() },
-                                    .default(Text("Snooze by 5 minutes")) {
-                                        withAnimation {
-                                            snoozeTappedReminder(by: 300)
-                                        }
-                                    },
-                                    .default(Text("Snooze by 30 minutes")) {
-                                        withAnimation {
-                                            snoozeTappedReminder(by: 1800)
-                                        }
-                                    },
-                                    .default(Text("Snooze by 1 hour")) {
-                                        withAnimation {
-                                            snoozeTappedReminder(by: 3600)
-                                        }
-                                    },
-                                    .default(Text("Snooze by 1 day")) {
-                                        withAnimation {
-                                            snoozeTappedReminder(by: 86400)
-                                        }
-                                    },
-                                    .default(Text("Edit/Reschedule")) {
-                                        viewModel.editReminder = true
-                                        viewModel.showingAddNewReminder = true
-                                    },
-                                    .cancel()
-                                ])
-                            }
-                    }
+        ScrollView {
+            LazyVStack {
+                ForEach(viewModel.allRemindersSorted) { reminder in
+                    ReminderListView(viewModel: viewModel, showingSnoozeActionSheet: $showingSnoozeActionSheet, reminder: reminder, height: 120)
+                        .alert(isPresented: $showingDeleteWarning) {
+                            Alert(
+                                title: Text("Are you sure you want to delete this?"),
+                                message: Text("There is no undo"),
+                                primaryButton: .destructive(Text("Delete")) {
+                                    withAnimation {
+                                        viewModel.deleteNotificationsForReminder(id: viewModel.tappedReminder!)
+                                    }
+                                },
+                                secondaryButton: .cancel())
+                        }
+                        .actionSheet(isPresented: $showingSnoozeActionSheet) {
+                            ActionSheet(title: Text("Select an option"), message: nil, buttons: [
+                                .destructive(Text("Delete '\(tappedReminderTitle)'")) { showingDeleteWarning.toggle() },
+                                .default(Text("Snooze by 5 minutes")) {
+                                    withAnimation {
+                                        snoozeTappedReminder(by: 300)
+                                    }
+                                },
+                                .default(Text("Snooze by 30 minutes")) {
+                                    withAnimation {
+                                        snoozeTappedReminder(by: 1800)
+                                    }
+                                },
+                                .default(Text("Snooze by 1 hour")) {
+                                    withAnimation {
+                                        snoozeTappedReminder(by: 3600)
+                                    }
+                                },
+                                .default(Text("Snooze by 1 day")) {
+                                    withAnimation {
+                                        snoozeTappedReminder(by: 86400)
+                                    }
+                                },
+                                .default(Text("Edit/Reschedule")) {
+                                    viewModel.editReminder = true
+                                    viewModel.showingAddNewReminder = true
+                                },
+                                .cancel()
+                            ])
+                        }
                 }
-                .padding(.horizontal, 10)
             }
-            .fullScreenCover(isPresented: $viewModel.showingAddNewReminder) {
-                AddReminderView(viewModel: viewModel, reminder: viewModel.editReminder ? findReminderWithId(id: viewModel.tappedReminder!) : nil)
-            }
-            .navigationTitle("NotNow").foregroundColor(Color(Colours.midnightBlue))
-            .toolbar {
-                ToolbarItem(placement: .primaryAction) {
-                    Button(action: {
-                        viewModel.showingAddNewReminder = true
-                    }) {
-                        Image(systemName: "plus.circle.fill")
-                            .font(.title)
-                    }
-                }
-                ToolbarItem(placement: .cancellationAction) {
-                    Button("Print notifications") {
-                        NotificationManager.shared.printAllNotifications()
-                    }
+            .padding(.horizontal, 10)
+        }
+        .fullScreenCover(isPresented: $viewModel.showingAddNewReminder) {
+            AddReminderView(viewModel: viewModel, reminder: viewModel.editReminder ? findReminderWithId(id: viewModel.tappedReminder!) : nil)
+        }
+        .navigationTitle("NotNow").foregroundColor(Color(Colours.midnightBlue))
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button(action: {
+                    viewModel.showingAddNewReminder = true
+                }) {
+                    Image(systemName: "plus.circle.fill")
+                        .font(.title)
                 }
             }
         }
-        .accentColor(Color(Colours.hotCoral))
     }
     
     func snoozeTappedReminder(by seconds: Double) {
